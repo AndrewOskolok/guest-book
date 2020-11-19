e47166a41dfc2dfdd93d6729d64926d567febc7b
Oscolok <oscolok7@gmail.com>
2020-11-19 18:23:18 +0200

    add gray background

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className={css.header}>
       <div className={css.header__container}>
-        <a href="/">
+        <a className={css.header__logo_link} href="/">
           <h1 className={css.header__title}>Guest Book</h1>
         </a>
         <p className={css.header__description}>
