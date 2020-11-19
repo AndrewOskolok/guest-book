d3b1fc915de1fdbb4335d7d756f83eb364924b4c
Oscolok <oscolok7@gmail.com>
2020-11-19 17:38:08 +0200

    add media ules

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
 import Main from "../Components/Main/Main";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
+import css from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={css.app}>
       <Header />
       <Main />
       <Footer />
